0e65c310d7e7ec3f76042a4538ee39b126735223
IvnoGood <afonsovic2009@gmail.com>
2024-08-07 21:17:15 +0200

    updated

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,48 @@
 let backgroundvideo = document.getElementById("myVideo");
+let played = true;
+let carouselTimeout;
+console.log(played);
 
-function playPause() { 
-  if (backgroundvideo.paused) 
-    backgroundvideo.play(); 
-  else 
-    backgroundvideo.pause(); 
-} 
+document.getElementById("video-controls").onclick = function() {
+  playPause();
+  carousel();
+}
+
+function playPause() {
+  if (played) {
+    played = false;
+  } else {
+    played = true;
+  }
+  console.log(played);
+}
+
+var myIndex = 0;
+
+function carousel() {
+  if (carouselTimeout) {
+    clearTimeout(carouselTimeout);
+  }
+
+  if (played) {
+    var i;
+    var x = document.getElementsByClassName("main-slideshow");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1; }
+    x[myIndex - 1].style.display = "block";
+    carouselTimeout = setTimeout(carousel, 9000);
+    console.log(myIndex);
+  }
+}
+
+// Start the carousel initially if `played` is true
+if (played) {
+  carousel();
+}
+
+function view(n){
+  
+}
